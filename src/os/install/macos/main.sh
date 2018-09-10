dc64a01cd5c7f7524e8c1f22db80a4bce21be292
Renhuan Ding <rhding@yitu-inc.intra>
2018-09-10 19:00:32 +0800

    [vim] 添加python自动格式化工具yapf

--- src/os/install/macos/main.sh
+++ src/os/install/macos/main.sh
@@ -6,22 +6,22 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-./xcode.sh
+#./xcode.sh
 ./homebrew.sh
 ./bash.sh
-./../nvm.sh
+#./../nvm.sh
 
-./browsers.sh
+#./browsers.sh
 ./compression_tools.sh
 ./git.sh
 ./gpg.sh
 ./image_tools.sh
 ./misc.sh
 ./misc_tools.sh
-./../npm.sh
+#./../npm.sh
 ./tmux.sh
 ./video_tools.sh
 ./../vim.sh
-./web_font_tools.sh
+#./web_font_tools.sh
 
 ./cleanup.sh
